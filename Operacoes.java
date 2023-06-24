a1f3312500db0e7af1aebf8a230c67ab0355e56e
gustavomgs <53432968+gustavomgs@users.noreply.github.com>
2023-06-24 11:49:15 -0300

    Update Operacoes.java

--- Operacoes.java
+++ Operacoes.java
@@ -1,20 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sp.senai.br.calculomedia;
 
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 public class Operacoes {
     
     public static Scanner teclado = new Scanner(System.in);
     
     public static double soma(double valor1, double valor2){
         return valor1 + valor2;
+    }
+    
+    public static double max(double[] vetor){
+        
+        double max = 0;
+        
+        for(int i = 0; i < vetor.length; i ++){
+            
+            if(vetor[i] > max){
+                max = vetor[i];
+            }
+            
+        }
+        
+        return max;
+    }
+    
+    public static double min(double[] vetor){
+        
+        double min = vetor[0];
+        
+        for(int i = 0; i < vetor.length; i ++){
+
+            if(vetor[i] < min && vetor[i] != 0){
+                min = vetor[i];
+            }
+            
+        }
+        
+        return min;
+    }
+    
+    public static double media(double[] vetor){
+        
+        double soma = 0;
+        
+        for(int i = 0; i < vetor.length; i++){
+            // soma = soma + vetor[i];
+
+            soma += vetor[i];          
+        }
+        
+        return soma / vetor.length;
     }
 }
